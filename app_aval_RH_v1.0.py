8b1e7dc7f1673ea16d73e911d96f01378178dbc4
DevENIND <naorespenind@hotmail.com>
2025-09-02 13:34:22 -0300

    Inclusão de Gráficos

--- app_aval_RH_v1.0.py
+++ app_aval_RH_v1.0.py
@@ -2342,6 +2342,6 @@ def main(page: ft.Page):
     page.add(stack) 
     
     
-#ft.app(target=main,view=ft.WEB_BROWSER)
+ft.app(target=main,view=ft.WEB_BROWSER)
 
-ft.app(target=main,view=ft.WEB_BROWSER, port=8000, host="0.0.0.0")
+#ft.app(target=main,view=ft.WEB_BROWSER, port=8000, host="0.0.0.0")
