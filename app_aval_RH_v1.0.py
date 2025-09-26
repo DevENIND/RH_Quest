00c57327d347b7beb73c3e6c231a9f52534747cf
DevENIND <naorespenind@hotmail.com>
2025-09-26 18:51:04 -0300

    Correção exportação e leitura de dados

--- app_aval_RH_v1.0.py
+++ app_aval_RH_v1.0.py
@@ -3276,7 +3276,7 @@ def main(page: ft.Page):
     page.add(stack) 
     
     
-ft.app(target=main,view=ft.WEB_BROWSER, port=8000)
+#ft.app(target=main,view=ft.WEB_BROWSER, port=8000)
 
 #Colocar sempre porta 8000
-#ft.app(target=main,view=ft.WEB_BROWSER, port=8000, host="0.0.0.0")
+ft.app(target=main,view=ft.WEB_BROWSER, port=8000, host="0.0.0.0")
